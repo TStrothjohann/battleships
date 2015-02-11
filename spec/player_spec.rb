43aadfab8aeb830869f284f43faf153a8520beb0
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-11 16:10:07 +0000

    Added a nice little test for you, bitch

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,20 @@
 require 'player'
-require 'board'
+require 'cell'
+
 
 describe Player do 
 
 	let(:player) {Player.new}
-	let(:board) {Board.new}
-
-	it'should be able to set a marker' do
 	
+  # let(:cella1){double :cell, status: :ship, location: :a1}
+  # let(:cella2){double :cell, status: :water, location: :a2}
+  # let(:cellb1){double :cell, status: :water, location: :b1}
+  # let(:cellb2){double :cell, status: :water, location: :b2}
+  let(:cell){double :cell}
 
+	it "should be able to change Cell1a's status to ship" do
+    cell.location = :a1
+    expect(cell.location).to eq(:a1)
 	end
 
 
