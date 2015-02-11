66c66766342d9194adb110eda1505237d78473b0
Thomas Strothjohann <thomas@strothjohann.net>
2015-02-11 12:04:31 +0000

    Deleted the old Board Class and started with a new test for you. Make the board hold cells.

--- lib/board.rb
+++ lib/board.rb
@@ -1,36 +1,4 @@
 class Board 
-  attr_accessor :cells
 
-  def initialize 
-      @cells = Hash.new
-  end
-
-  def make_board(width)
-    @width = width
-    @cells[:a1] = :water
-    @cells[:a2] = :water
-    @cells[:b1] = :water
-    @cells[:b2] = :water
-  end
-
-  def cell_count
-		@cells.length
-  end
-
-  def place_ship(cell)
-    @cells[cell] = :ship
-  end
-
-  def cell_lookup(cell)
-    @cells[cell] 
-  end
-
-  def set_marker(cell)
-      if cell_lookup(cell) == :ship
-         @cells[cell] = :hit
-       else
-        @cells[cell] = :missed
-       end
-  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,31 +2,13 @@ require 'board'
 
 describe Board do
   let(:board){Board.new}
+  let(:cell){double :cell, status: :water}
+  
 
-  it "should know how many cells it has" do
-    board.make_board(2)
-    expect(board.cell_count).to eq(4)
+  it "should be able to hold cells" do
+    expect(board.cell_count).to eq(0)
+    board.hold(cell)
+    expect(board.cell_count).to eq(1)
   end
-
-  it "should be able to place a ship" do
-    board.place_ship(:a1)
-    expect(board.cell_lookup(:a1)).to eq(:ship)
-  end
-
-  it "should be able to place miss-marker" do
-    board.set_marker(:a2)
-    expect(board.cell_lookup(:a2)).to eq(:missed)
-  end
-
-  it 'should be able to place a hit-marker' do
-    board.place_ship(:a1)
-    board.set_marker(:a1)
-    expect(board.cell_lookup(:a1)).to eq(:hit)
-  end
-
-  # it "should not set a marker that has allready been set" do
-  #   board.set_marker(:b2)
-  #   expect{board.set_marker(:b2)}.to eq("You have asked me that, stupid!")
-  # end
 
 end
